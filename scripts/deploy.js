244faf6174376c6e69116849c6d57f9b3e98c536
Luke Sheppard <sheppiegit@gmail.com>
2023-04-25 17:18:40 -0400

    Added a deploy script in the script folder

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+//imports
+const { ethers } = require('hardhat');
+
+async function main() {
+  const SimpleStorageFactory = await ethers.getContractFactory('SimpleStorage');
+  console.log('Deploy contract');
+
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  await simpleStorage.deployed();
+
+  console.log(`Deployed contract to: ${simpleStorage.address}`);
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
